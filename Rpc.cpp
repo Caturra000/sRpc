707e0caefd25089763a5e6c8f79f1d83d6bdf6fc
Caturra <wasdjkl233@gmail.com>
2021-01-09 20:21:33 +0800

    add function call demo

--- Rpc.cpp
+++ Rpc.cpp
@@ -9,6 +9,17 @@ using namespace mutty;
 using namespace vsjson;
 
 
+struct Point {
+    int x, y, z;
+    operator Json() { return Json::array(x, y, z); }
+    Point(int x, int y, int z): x(x), y(y), z(z) {}
+    Point(const std::vector<vsjson::Json> &array)
+        : x(array[0].to<int>()),
+          y(array[1].to<int>()),
+          z(array[2].to<int>()) {}
+    Point() = default;
+};
+
 
 int add(int a, int b) {
     return a+b;
@@ -16,6 +27,10 @@ int add(int a, int b) {
 
 std::string append(std::string a, std::string b) {
     return a+b;
+}
+
+Point pointAdd(Point a, Point b) {
+    return {a.x+b.x, a.y+b.y, a.z+b.z};
 }
 
 void minus(int a, int b) {
@@ -28,9 +43,12 @@ int main() try {
     server.bind("add", add);
     server.bind("minus", minus);
     server.bind("append", append);
-    auto json = server.netCall("append", Json::array("100", "200"));
-
-    std::cout << json << std::endl;
+    server.bind("p", pointAdd);
+    auto json = server.netCall("p", Json::array(Point{1,2,3}, Point{4,5,6}));
+    auto point = json.to<Point>();
+    auto json2 = server.netCall("append", Json::array("abc", "def"));
+    std::cout << point.x << std::endl;
+    std::cout << json2 << std::endl;
     
     // RpcClient client(InetAddress("127.0.0.1", 23333));
     // auto future = client.call<int>("add", 1, 2);
