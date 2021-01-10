90f209cde7c1f3faa8ebf9b84b7134b2053ddbbe
Caturra <wasdjkl233@gmail.com>
2021-01-10 13:18:19 +0800

    add submodule

--- depends/mutty/mutty.hpp
+++
@@ -1,5 +0,0 @@
-#ifndef __MUTTY__
-#define __MUTTY__
-#include "app/Client.h"
-#include "app/Server.h"
-#endif
--- depends/vsjson/vsjson.hpp
+++
@@ -1,5 +0,0 @@
-#ifndef __VS_JSON__
-#define __VS_JSON__
-#include "src/Json.h"
-#include "src/Parser.h"
-#endif
